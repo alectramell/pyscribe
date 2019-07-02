7037096ce74c262da99ae23b8a9ffe2e59c9c2d2
alectramell <alectramell@gmail.com>
2019-07-02 23:25:56 +0000

    scribe

---
+++ scribe.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+def rev(xvar):
+
+	return(xvar[::-1])
+
+def hex(xvar):
+
+	a = '61'
+	b = '62'
+	c = '63'
+	d = '64'
+	e = '65'
+	f = '66'
+	g = '67'
+	h = '68'
+	i = '69'
+	j = '6A'
+	k = '6B'
+	l = '6C'
+	m = '6D'
+	n = '6E'
+	o = '6F'
+	p = '70'
+	q = '71'
+	r = '72'
+	s = '73'
+	t = '74'
+	u = '75'
+	v = '76'
+	w = '78'
+	x = '79'
+	y = '7A'
+	z = '7B'
+	A = '41'
+	B = '42'
+	C = '43'
+	D = '44'
+	E = '45'
+	F = '46'
+	G = '47'
+	H = '48'
+	I = '49'
+	J = '4A'
+	K = '4B'
+	L = '4C'
+	M = '4D'
+	N = '4E'
+	O = '4F'
+	P = '50'
+	Q = '51'
+	R = '52'
+	S = '53'
+	T = '54'
+	U = '55'
+	V = '56'
+	W = '57'
+	X = '58'
+	Y = '59'
+	Z = '5A'
+
+	xout = xvar.replace('a', a).replace('b', b).replace('c', c).replace('d', d).replace('e', e).replace('f', f).replace('g', g).replace('h', h).replace('i', i).replace('j', j).replace('k', k).replace('l', l).replace('m', m).replace('n', n).replace('o', o).replace('p', p).replace('q', q).replace('r', r).replace('s', s).replace('t', t).replace('u', u).replace('v', v).replace('w', w).replace('x', x).replace('y', y).replace('z', z)
+
+	return(xout)
